0fa2e5f1a9eb3fad08be28d855ccc943097ff220
Samuel Paget <sjapaget@gmail.com>
2022-08-21 15:51:58 +0200

    Add lists#new and lists#create with new view - Add ability to create new lists

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'lists#index'
 
-  resources :lists, only: [:index, :show]
+  resources :lists, only: [:index, :show, :new, :create]
 end
