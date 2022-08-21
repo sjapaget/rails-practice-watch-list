81184c22dab15c786ba7286fffd51124fe300168
Samuel Paget <sjapaget@gmail.com>
2022-08-21 13:38:35 +0200

    Add seed for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+puts 'Cleaning the DB'
+Movie.destroy_all
+
+puts 'Fetching movies'
+
+movies_serialized = URI.open('https://tmdb.lewagon.com/movie/top_rated?').read
+
+movies = JSON.parse(movies_serialized)["results"]
+
+movies.each do |movie|
+  puts 'Creating new movie'
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+  puts "Created #{movie["title"]}"
+end
+
+puts "All done"
