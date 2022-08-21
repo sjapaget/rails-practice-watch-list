39d73d436466d4ff162f72b959431a560b6e1b92
Samuel Paget <sjapaget@gmail.com>
2022-08-21 15:00:16 +0200

    Add list#index with controller method and view

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  DEFAULT_IMG = "https://images.unsplash.com/photo-1585647347129-7aebb8689a78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
 end
