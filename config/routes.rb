829cc4d5e3c662e63dc0101ca0a0073c4a3c76b5
Samuel Paget <sjapaget@gmail.com>
2022-08-21 19:00:14 +0200

    Add display of reviews on list show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
