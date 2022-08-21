aec32e38e708d92cf2c9321500b668379ac75f53
Samuel Paget <sjapaget@gmail.com>
2022-08-21 15:21:44 +0200

    Add list#show with view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'lists#index'
 
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show]
 end
